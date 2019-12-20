99e1183e43f9807d16f64f75d40bc4797234e356
pzrr@qq.com <pzrr@qq.com>
2019-12-20 16:32:49 +0800

    #### Version 0.9.3 * Opt: 完善config模块，优化config注入模式 * Opt: 增加ConfigHandle函数定义,用于应用自定义config加载实现(如从redis\mysql等源读取配置) * feature: 新增LoadFileConfig用于逐步替换原LoadConfig,主要用于读取xml\json\yaml格式配置文件 * feature: 新增LoadConfigHandler用于应用自定义配置加载方式的注入 * Opt: config完善注释 * 2019-12-20 09:00 at ShangHai

--- config.go
+++ config.go
@@ -19,15 +19,15 @@ type (
 		} `xml:"global" yaml:"global"`
 
 		Tasks []struct {
-			TaskID      string `xml:"taskid,attr" yaml:"taskid"`
-			IsRun       bool   `xml:"isrun,attr" yaml:"isrun"`
-			TaskType    string `xml:"type,attr" yaml:"type"`
-			DueTime     int64  `xml:"duetime,attr" yaml:"duetime"` //开始任务的延迟时间（以毫秒为单位），如果<=0则不延迟
-			Interval    int64  `xml:"interval,attr" yaml:"interval"`
-			Express     string `xml:"express,attr" yaml:"express"`
-			QueueSize   int64  `xml:"queuesize,attr" yaml:"queuesize"`
-			HandlerName string `xml:"handlername,attr" yaml:"handlername"`
-			HandlerData string `xml:"handlerdata,attr" yaml:"handlerdata"`
+			TaskID      string `xml:"taskid,attr" yaml:"taskid"`           //task编号，需唯一
+			IsRun       bool   `xml:"isrun,attr" yaml:"isrun"`             //标识是否允许task执行,默认为false,如设为flash,则启动后不执行task
+			TaskType    string `xml:"type,attr" yaml:"type"`               //Task类型,目前支持loop、cron、queue
+			DueTime     int64  `xml:"duetime,attr" yaml:"duetime"`         //开始任务的延迟时间（以毫秒为单位），如果<=0则不延迟
+			Interval    int64  `xml:"interval,attr" yaml:"interval"`       //loop类型下,两次Task执行之间的间隔,单位为毫秒
+			Express     string `xml:"express,attr" yaml:"express"`         //cron类型下,task执行的时间表达式，具体参考readme
+			QueueSize   int64  `xml:"queuesize,attr" yaml:"queuesize"`     //queue类型下,queue初始长度
+			HandlerName string `xml:"handlername,attr" yaml:"handlername"` //Task对应的HandlerName,需使用RegisterHandler进行统一注册
+			HandlerData string `xml:"handlerdata,attr" yaml:"handlerdata"` //Task对应的自定义数据,可在配置源中设置
 		} `xml:"tasks>task" yaml:"tasks"`
 	}
 
